77065280c35b00a14b13493ca24b834a80f5fde4
Agasthi98 <sankalana98@gmail.com>
2023-10-06 15:28:44 +0530

    some changes in styles

--- src/components/technologies/technologies.jsx
+++ src/components/technologies/technologies.jsx
@@ -10,43 +10,50 @@ import "./technologies.scss";
 
 const Technologies = () => {
   return (
-    <div className="slider">
-      <Splide
-        className="splide"
-        options={{
-          type: "loop",
-          perPage: 4,
-          focus: "center",
-        }}
-        aria-label="My Favorite Images"
-      >
-        <SplideSlide>
-          <img src={python} alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={c} alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={arduino} alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={truck} alt="Image 2" />
-        </SplideSlide>
+    <>
+      <div className="title">Technologies</div>
+      <div className="slider">
+        <Splide
+          className="splide"
+          options={{
+            type: "loop",
+            perPage: 5,
+            focus: "center",
+          }}
+          aria-label="My Favorite Images"
+        >
+          <SplideSlide>
+            <img src={python} alt="Image 1" />
+            <div className="text-icon">Python</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img src={c} alt="Image 2" />
+            <div className="text-icon">C</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img src={arduino} alt="Image 2" />
+            <div className="text-icon">Arduino</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img src={truck} alt="Image 2" />
+            <div className="text-icon">Truck</div>
+          </SplideSlide>
 
-        <SplideSlide>
-          <img src={python} alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={c} alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={arduino} alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={truck} alt="Image 2" />
-        </SplideSlide>
-      </Splide>
-    </div>
+          <SplideSlide>
+            <img src={python} alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={c} alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={arduino} alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={truck} alt="Image 2" />
+          </SplideSlide>
+        </Splide>
+      </div>
+    </>
   );
 };
 
