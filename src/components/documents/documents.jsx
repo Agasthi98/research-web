f6a7ee2ad8c4efa783ba8e35612f6d0cfeaa14a8
Agasthi98 <sankalana98@gmail.com>
2023-10-11 14:37:18 +0530

    prentation sides okay

--- src/components/documents/documents.jsx
+++ src/components/documents/documents.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./documents.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile, faFileLines } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFilePowerpoint,
+  faFileLines,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Documents = () => {
   return (
@@ -157,6 +160,65 @@ const Documents = () => {
                 <button className="view-btn">
                   <b>View</b>
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="presentation-side">
+          <div className="doc-title">Presentations</div>
+          <div className="document-body">
+            <div className="document-wrapper">
+              <div className="doc-items">
+                <div className="row">
+                  <FontAwesomeIcon icon={faFilePowerpoint} beat />
+                  <div className="document-name">
+                    PROJECT PROPOSAL PRESENTATION
+                  </div>
+                  <div className="document-description">
+                    Take a look at the Topic Assessment Form submitted at the
+                    begining of the Research Project which contains the
+                    individual work distribution.
+                  </div>
+                  <div className="submitted-date">
+                    Group - Submitted on 28/12/2021
+                  </div>
+                  <button className="view-btn">
+                    <b>View</b>
+                  </button>
+                </div>
+                <div className="row">
+                  <FontAwesomeIcon icon={faFilePowerpoint} beat />
+                  <div className="document-name">PROGRESS PRESENTATION - I</div>
+                  <div className="document-description">
+                    Take a look at the Topic Assessment Form submitted at the
+                    begining of the Research Project which contains the
+                    individual work distribution.
+                  </div>
+                  <div className="submitted-date">
+                    Group - Submitted on 28/12/2021
+                  </div>
+                  <button className="view-btn">
+                    <b>View</b>
+                  </button>
+                </div>
+                <div className="row">
+                  <FontAwesomeIcon icon={faFilePowerpoint} beat />
+                  <div className="document-name">
+                    PROGRESS PRESENTATION - II
+                  </div>
+                  <div className="document-description">
+                    Take a look at the Topic Assessment Form submitted at the
+                    begining of the Research Project which contains the
+                    individual work distribution.
+                  </div>
+                  <div className="submitted-date">
+                    Group - Submitted on 28/12/2021
+                  </div>
+                  <button className="view-btn">
+                    <b>View</b>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
